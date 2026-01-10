29eb35c473b1a49b69b045bf7e4e6d77bb598907
Miguel Ángel Montilla Pérez <miguelmontilla.1936@gmail.com>
2026-01-09 19:33:36 -0500

    Se separan las dependencias para hilt de retrofit en un nuevo modulo.

---
+++ app/src/main/java/com/skyfallen/instadev/di/NetworkModule.kt
@@ -0,0 +1,38 @@
+package com.skyfallen.instadev.di
+
+import com.skyfallen.instadev.BuildConfig
+import com.skyfallen.instadev.data.datasource.api.ApiServices
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+import retrofit2.converter.kotlinx.serialization.asConverterFactory
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    fun provideApiServices(retrofit: Retrofit): ApiServices {
+        return retrofit.create(ApiServices::class.java)
+    }
+
+    @Provides
+    fun provideRetrofit(json: Json): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BuildConfig.BASE_URL)
+            .addConverterFactory(json.asConverterFactory("application/json; charset=utf-8".toMediaType()))
+            .build()
+    }
+
+    @Provides
+    fun provideJson(): Json {
+        return Json {
+            ignoreUnknownKeys = true
+            isLenient = true
+        }
+    }
+}
--- app/src/main/java/com/skyfallen/instadev/di/RepositoryModule.kt
+++ app/src/main/java/com/skyfallen/instadev/di/RepositoryModule.kt
@@ -1,6 +1,5 @@
 package com.skyfallen.instadev.di
 
-import com.skyfallen.instadev.BuildConfig
 import com.skyfallen.instadev.data.auth.login.repository.AuthRepositoryImpl
 import com.skyfallen.instadev.data.datasource.api.ApiServices
 import com.skyfallen.instadev.domain.auth.login.repository.AuthRepository
@@ -8,10 +7,6 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import kotlinx.serialization.json.Json
-import okhttp3.MediaType.Companion.toMediaType
-import retrofit2.Retrofit
-import retrofit2.converter.kotlinx.serialization.asConverterFactory
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -20,26 +15,5 @@ object RepositoryModule {
     @Provides
     fun provideLoginRepository(api: ApiServices) : AuthRepository {
         return AuthRepositoryImpl(api)
-    }
-
-    @Provides
-    fun provideApiServices(retrofit: Retrofit): ApiServices {
-        return retrofit.create(ApiServices::class.java)
-    }
-
-    @Provides
-    fun provideRetrofit(json: Json): Retrofit {
-        return Retrofit.Builder()
-            .baseUrl(BuildConfig.BASE_URL)
-            .addConverterFactory(json.asConverterFactory("application/json; charset=utf-8".toMediaType()))
-            .build()
-    }
-
-    @Provides
-    fun provideJson(): Json {
-        return Json {
-            ignoreUnknownKeys = true
-            isLenient = true
-        }
     }
 }
